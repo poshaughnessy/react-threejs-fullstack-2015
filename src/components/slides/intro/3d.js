141da6ec3ddda6b4302f9be6ded6e83ce6c0959f
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2015-10-26 19:51:23 +0000

    3D slide styling

--- src/components/slides/intro/3d.js
+++ src/components/slides/intro/3d.js
@@ -6,8 +6,8 @@ class SlideContents extends React.Component {
     render() {
 
         return (
-            <div className="contents 3d">
-                <h2>Say hello to the 3rd dimension</h2>
+            <div className="contents threed">
+                <h2>Hello, 3rd dimension</h2>
             </div>
         );
 
