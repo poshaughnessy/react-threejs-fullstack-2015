719c4912cb81a2d8b658e0f64e29e1715a0ea7d0
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2015-10-27 10:58:14 +0000

    Change copy

--- src/components/slides/react/why-react.js
+++ src/components/slides/react/why-react.js
@@ -5,13 +5,13 @@ class SlideContents extends React.Component {
 
     render() {
 
-        // TODO if time, replace image with working in-place demo?
         return (
             <div className="contents why-react">
                 <h2>Why React?</h2>
                 <ul>
                     <li>Simpler, declarative code</li>
-                    <li>Fast UIs by default</li>
+                    <li>Components encourages modular code</li>
+                    <li>Fast UIs by default (Virtual DOM)</li>
                 </ul>
             </div>
         );
