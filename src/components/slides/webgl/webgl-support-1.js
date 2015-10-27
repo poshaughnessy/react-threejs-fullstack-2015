a8c2670f3921d37c5fedf72cb22a68fe973ebc80
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2015-10-27 23:21:14 +0000

    Add WebGL support legend

--- src/components/slides/webgl/webgl-support-1.js
+++ src/components/slides/webgl/webgl-support-1.js
@@ -9,6 +9,7 @@ class SlideContents extends React.Component {
             <div className="contents webgl-support-1">
                 <h2>WebGL Support</h2>
                 <img src="/images/webgl-support-caniuse.png" alt="WebGL support chart"/>
+                <p className="caption"><span className="yellow-green"></span> = depends on graphics card / drivers</p>
             </div>
         );
 
