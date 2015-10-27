0054297d713557b672161c1fbb688c740d976d9c
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2015-10-27 12:05:21 +0000

    Slide tweaks

--- src/components/slides/react/react-example-2.js
+++ src/components/slides/react/react-example-2.js
@@ -11,28 +11,34 @@ class SlideContents extends React.Component {
             <div className="contents top to-do-list-code">
                 <Highlight className="javascript">
                 {`var TodoList = React.createClass({
+
   render: function() {
     var createItem = function(itemText, index) {
       return <li key={index + itemText}>{itemText}</li>;
     };
     return <ul>{this.props.items.map(createItem)}</ul>;
   }
+
 });
 
 
 var TodoApp = React.createClass({
+
   getInitialState: function() {
     return {items: [], text: ''};
   },
+
   onChange: function(e) {
     this.setState({text: e.target.value});
   },
+
   handleSubmit: function(e) {
     e.preventDefault();
     var nextItems = this.state.items.concat([this.state.text]);
     var nextText = '';
     this.setState({items: nextItems, text: nextText});
   },
+
   render: function() {
     return (
       <div>
